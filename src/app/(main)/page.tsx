43bee2b0bf9ab6d96cf8eb0fcb9d408b93320907
yuu-1204 <104064236+uyukigakata@users.noreply.github.com>
2025-03-20 09:30:07 +0900

    packege error revision

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { useState, useEffect } from "react";
 import profileData from "@/data/profile.json";
 
