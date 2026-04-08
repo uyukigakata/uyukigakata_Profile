9906b19017a53a68019b65d84395d6ff959a0c82
yuu-1204 <104064236+uyukigakata@users.noreply.github.com>
2026-04-08 18:45:56 +0900

    活動履歴追加

--- src/app/(main)/(activities)/activities/page.tsx
+++ src/app/(main)/(activities)/activities/page.tsx
@@ -1,10 +1,20 @@
 import ActivityTimeline from "@/components/ActivityTimeline";
 import activities from "@/data/activities.json";
 
+
+type Activity = {
+  date: string;
+  title: string;
+  description?: string;
+  side: "left" | "right";
+};
+
 export default function ActivitiesPage() {
+  const typedActivities = activities as Activity[];
+
   return (
     <main className="min-h-screen bg-gray-100 py-10">
-      <ActivityTimeline items={activities} />
+      <ActivityTimeline items={typedActivities} />
     </main>
   );
 }
